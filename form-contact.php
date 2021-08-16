c70b683a806b3bbe7aada24b7fdbd3a75f2673b5
Hindra Estrada <hindrave@gmail.com>
2021-08-15 20:24:54 -0500

    inputs -> buttons

--- form-contact.php
+++ form-contact.php
@@ -1,58 +1,20 @@
 <?php
-    $email_to = "hindrave@gmail.com";
-    $email_subject = "Contacto desde la web";
+$affair = $_POST['affair'];
+$name = $_POST['name'];
+$mail = $_POST['mail'];
+$msg = $_POST['msg'];
 
-    function died($error) {
-        // Error messages
-        echo "Lo sentimos, hubo un error en sus datos y el formulario no puede ser enviado en este momento. ";
-        echo "Detalle de los errores.<br /><br />";
-        echo $error."<br /><br />";
-        echo "Porfavor corrija estos errores e inténtelo de nuevo.<br /><br />";
-        die();
-    }
-    if(!isset($_POST['asunto']) ||
-        !isset($_POST['nombre']) ||
-        !isset($_POST['email']) ||
-        !isset($_POST['message'])) {
-        die('Lo sentimos pero parece haber un problema con los datos enviados.');
-    }
-    $affair = $_POST['asunto']; // required
-    $name = $_POST['nombre']; // required
-    $email_from = $_POST['email']; // required
-    $message = $_POST['mensaje']; // required
-    $error_message = "";//
-    //Verifying email
-   $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
-   if(!preg_match($email_exp,$email_from)) {
-     $error_message .= 'La dirección de correo proporcionada no es válida.<br />';
-   }
-   //Verifying strings
-   $string_exp = "/^[A-Za-z .'-]+$/";
-   if(!preg_match($string_exp,$affair)) {
-     $error_message .= 'El formato del asunto no es válido<br />';
-   }
-   if(!preg_match($string_exp,$name)) {
-     $error_message .= 'El formato del nombre no es válido<br />';
-   }
-   if(strlen($message) < 2) {
-     $error_message .= 'El formato del texto no es válido.<br />';
-   }
-   if(strlen($error_message) > 0) {
-     die($error_message);
-   }
-   //Body message
-   $email_message = "Contenido del Mensaje.\n\n";
-   function clean_string($string) {
-     $bad = array("content-type","bcc:","to:","cc:","href");
-     return str_replace($bad,"",$string);
-   }
-   $email_message = "Asunto: ".clean_string($asunto)."\n";
-   $email_message .= "Nombre: ".clean_string($nombre)."\n";
-   $email_message .= "Email: ".clean_string($email_from)."\n";
-   $email_message .= "Mensaje: ".clean_string($message)."\n";
-    //Creating mail headers
-    $headers = 'From: '.$email_from."\r\n".
-    'Reply-To: '.$email_from."\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-    @mail($email_to, $email_subject, $email_message, $headers);
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+$message = "Saludos Staff de Lucsax. \n\n Mi nombre es:  " . $name . ", los contacto a través de su plataforma en línea lucsax.net. \r\n\n";
+$message .= $_POST['msg'] . " \r\n\n";
+$message .= "\n Mensaje enviado el " . date('d/m/Y', time());
+
+$para = 'soluciones@lucsax.net';
+
+mail($para, $affair, utf8_decode($message), $header);
+
+header("Location:index.html");
 ?>
